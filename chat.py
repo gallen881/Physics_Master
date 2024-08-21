376ec9f06be2fa5d38005328aca6eb1b06964621
Gallen Wang <gallen.yes@gmail.com>
2024-08-21 10:39:35 +0800

    update API

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 from llama_cpp import Llama
+import urllib.parse
 import json
 import requests
 import sys
@@ -7,17 +8,31 @@ import time
 
 def wolframalpha_query(question):
     print('Physics Master is using WolframAlpha...', end='\r')
-    api_url = "https://api.wolframalpha.com/v1/spoken"
-    params = {
-        "appid": WOLFRAMALPHA_KEY,
-        "i": question
-    }
-    r = requests.get(api_url, params=params)
-    sys.stdout.write("\033[K") 
+    query = urllib.parse.quote_plus('calculate ' + question)
+    query_url = f"http://api.wolframalpha.com/v2/query?" \
+                f"appid={WOLFRAMALPHA_KEY}" \
+                f"&input={query}" \
+                f"&includepodid=Result" \
+                f"&output=json"
+    r = requests.get(query_url)
+    sys.stdout.write("\033[K")
     if r.status_code == 200:
-        return r.text
+        try:
+            return r.json()["queryresult"]["pods"][0]["subpods"][0]["plaintext"]
+        except:
+            print(question)
+            print(r.json()["queryresult"])
     else:
-        return 'Error: ' + r.text + '. Please ONLY output "<|wolfamalpha|>question have to calculate here". For example, "<|wolframalpha|>x^2+2x+1=0". Here is the wrong example: "Thank you for the reminder. Here is the correct input:\n\n<|wolframalpha|>x^2+2x+1=0"'
+        return ''
+
+def prompt_from_messages(messages):
+    prompt = ''
+    for message in messages:
+        prompt += f"<|start_header_id|>{message['role']}<|end_header_id|>\n\n"
+        prompt += f"{message['content']}<|eot_id|>"
+    prompt = prompt[:-10]
+    return prompt
+        
     
 with open('config.json') as file:
     keys = json.load(file)
@@ -34,7 +49,7 @@ llm = Llama.from_pretrained(
 messages = [
     {
         'role': 'system',
-        'content':'You are a professional physics master, you can answer the physics questions you know.' + ' If you are not sure about the answer, especially problems that require calculation or internet, you must use WolframAlpha. To use WolframAlpha, please ONLY output "<|wolframalpha|>" in the message, and then ONLY output the question you want to use WolframAlpha to calculate. For example, "<|wolframalpha|>x^2+2x+1=0", other information is not necessary. After system get the answer from WolframAlpha, you can continue to answer the question. If you have new question, please output "<|wolframalpha|>your question here" again.' if WOLFRAMALPHA_KEY else ''
+        'content':'You are a professional physics master, you can answer the physics questions you know.' + ' If you are not sure about the answer, especially problems that require calculation or internet, you must use WolframAlpha. To use WolframAlpha, please ONLY output "<|wolframalpha|>" in the message, and then ONLY output the calculaion formula you want to use WolframAlpha to calculate. For example, "<|wolframalpha|>x^2+2x+1=0" or "<|wolframalpha|>1024*512/3", a <|wolframalpha|> tag can ONLY use in a calculation formula, other information is not necessary. <|wolframalpha|x^2+2x+1=0|> is a wrong example. After system get the answer from WolframAlpha, you can continue to answer the question. If you have new question, please output "<|wolframalpha|>your question here" again.' if WOLFRAMALPHA_KEY else ''
     }
 ]
 
@@ -53,7 +68,20 @@ else:
 chat_time = time.time()
 chat_history.append({"time": chat_time, "messages": None})
 user_inputing = True
+use_wolframalpha = False
 while True:
+    if use_wolframalpha:
+        use_wolframalpha = False
+        # print('Asking WolframAlpha: ' + query)
+        query = ''
+        for i in wolf_answer.split():
+            print(i, end=' ', flush=True)
+        response = llm.create_completion(
+            prompt=prompt_from_messages(messages)
+        )
+        print(response['choices'][0]['text'])
+        messages[-1]['content'] += response['choices'][0]['text']
+
     chat_history[-1]['messages'] = messages
     with open('chat_history.json', 'w', encoding='utf8') as file:
         json.dump(chat_history, file, indent=4)
@@ -62,18 +90,46 @@ while True:
         user_message = {'role': 'user', 'content': user_input}
         messages.append(user_message)
     print('Physics Master is thinking...', end='\r')
-    r = llm.create_chat_completion(
-        messages=messages
+    response = llm.create_chat_completion(
+        messages=messages,
+        stream=True
     )
+    # print(response)
     sys.stdout.write("\033[K")
-    llm_message = r['choices'][0]['message']
-    messages.append(llm_message)
-    if '<|wolframalpha|>' in llm_message['content']:
-        question = llm_message['content'].replace('<|wolframalpha|>', '').strip()
-        answer = wolframalpha_query(question)
-        if not answer.startswith('Error'): print('\033[95mWolframAlpha: \033[0m', answer)
-        messages.append({'role': 'wolframalpha', 'content': answer})
-        user_inputing = False
-        continue
-    print('\033[96mPhysics Master: \033[0m', llm_message['content'])
-    user_inputing = True
+    wolf_token = ''
+    for chunk in response:
+        delta = chunk['choices'][0]['delta']
+        if 'role' in delta:
+            messages.append({'role': delta['role'], 'content': ''})
+            print("\033[95mPhysics Master: \033[0m", end='', flush=True)
+        elif 'content' in delta:
+            token = delta['content']
+            if '<|wolframalpha|>' in wolf_token:
+                use_wolframalpha = True
+                query = wolf_token.replace('<|wolframalpha|>', '')
+                wolf_token = ''
+
+            if use_wolframalpha:
+                if '\n' in token:
+                    wolf_answer = wolframalpha_query(query)
+                    messages[-1]['content'] += wolf_answer
+                    break
+                else:
+                    query += token
+                continue
+
+
+            wolf_token += token
+            if wolf_token == '<|wolframalpha|>'[:len(wolf_token)] or '<|wolframalpha|>' in wolf_token:
+                continue
+            else:
+                token = wolf_token
+                wolf_token = ''
+
+            messages[-1]['content'] += token
+            print(token, end="", flush=True)
+    print()
+    if use_wolframalpha and query:
+        wolf_answer = wolframalpha_query(query)
+        messages[-1]['content'] += wolf_answer
+        query = ''
